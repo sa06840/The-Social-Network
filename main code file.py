55b87b70f5a06d2e77b68cdd7f00569ac679e54a
sa06840 <sa06840@st.habib.edu.pk>
2021-05-10 18:00:02 +0500

    Create main code file
    File contains functions for creating a social network from a csv file.

---
+++ main code file.py
@@ -0,0 +1,30 @@
+
+#Mini Facebook
+
+#Constructing Social Network
+
+import csv
+
+def readcsv(filename):
+    matrix = []
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            matrix.append(row)
+    return matrix
+
+def constructing_social_network(matrix):
+    social_network = []
+    for i in matrix[0]:
+        if i != '':
+            social_network[i] = []
+    for i in range(1, len(matrix)):
+        for j in range(1, len(matrix)):
+            if int(matrix[i][j]) != 0:
+                social_network[matrix[i][0]].append((matrix[0][j], int(matrix[i][j])))
+    return social_network
+
+filename = 'friendships.csv'
+matrix = readcsv(filename)
+print('Social Network: ')
+print(constructing_social_network(matrix), '\n')
