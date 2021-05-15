7f1c0b81968e24efe618d21ab99c49df4ee85205
sa06840 <sa06840@st.habib.edu.pk>
2021-05-15 10:44:07 +0500

    Added 'get_recommended_friends' function along with 3 helper functions.
    The 'get_profile_in_list' function takes a user's name as an input and returns their profile in a list form. The 'is_empty' function checks if the input list is empty or not. The 'get_neighbours' function takes a node as an input and returns its adjacent nodes. The 'get_recommended_friends' function takes a user's name as an input and returns their recommended friends in order from most recommended to least recommended on the basis of their mutual friends, age, country, department and hobbies.

--- main code file.py
+++ main code file.py
@@ -2,6 +2,29 @@
 #Mini Facebook
 
 
+#Helper Functions
+
+def get_profile_in_list(profiles, user):
+    lst = []
+    for i in profiles[user]:
+        elem = i[1].split(', ')
+        for j in elem:
+            lst.append(j)
+    return lst
+
+def is_empty(lst):
+    if len(lst) == 0:
+        return True
+    else:
+        return False
+
+def get_neighbours(G, node):
+    neighbours = []
+    for i in G[node]:
+        neighbours.append(i)
+    return neighbours
+
+
 
 #Constructing Social Network
 
@@ -104,3 +127,51 @@ def get_user_profile(profiles, user):
 
 profiles = constructing_profiles(matrix_of_profiles)
 get_user_profile(profiles, user)
+print('\n')
+
+
+
+#Recommending Friends
+
+def get_recommended_friends(social_network, profiles, user):
+    cost = {}
+    unknown = []
+    recommended_friends = []
+    for node in social_network:
+        cost[node] = 100000
+        unknown.append(node)
+    cost[user] = 0
+    while not(is_empty(unknown)):
+        lowest = None
+        for node in unknown:
+            if lowest is None:
+                lowest = node
+            elif cost[node] < cost[lowest]:
+                lowest = node
+        unknown.remove(lowest)
+        for neighbour in get_neighbours(social_network, lowest):
+            c1 = cost[lowest] + neighbour[1]
+            c2 = cost[neighbour[0]]
+            if c1 < c2:
+                cost[neighbour[0]] = c1
+    user_profile = get_profile_in_list(profiles, user)
+    for i in cost:
+        counter = 0
+        i_profile = get_profile_in_list(profiles, i)
+        for j in user_profile:
+            if j in i_profile:
+                counter = counter + 1
+        new_cost = cost[i] - counter
+        cost[i] = new_cost
+    user_friends = get_friends_list(social_network, user)
+    user_friends.append(user)
+    for i in cost:
+        if i not in user_friends:
+            recommended_friends.append((i, cost[i]))
+    recommended_friends = sorted(recommended_friends, key=lambda item: item[1])
+    for i in range(1, len(recommended_friends)+1):
+        recommended_friend = recommended_friends[i-1][0]
+        print(str(i) + '. ' + recommended_friend)
+
+print('Recommended Friends for ' + str(user) + ': ')
+get_recommended_friends(social_network, profiles, user)
